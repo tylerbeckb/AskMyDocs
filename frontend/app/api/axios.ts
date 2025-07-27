bfaf24e464289be6b50c582741e96318175975f6
Tyler Brown <tylerbbrown@icloud.com>
2025-07-27 09:24:14 +0100

    fix: API base URL

--- frontend/app/api/axios.ts
+++ frontend/app/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:800',
+  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000',
     headers: {
         'Content-Type': 'application/json',
     },
