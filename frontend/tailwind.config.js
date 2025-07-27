bf6b60f42ec925d8fae542c25fc03d3e13848bc2
Tyler Brown <tylerbbrown@icloud.com>
2025-07-27 09:33:50 +0100

    feat: bounce animation

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -8,4 +8,18 @@ module.exports = {
       extend: {},
     },
     plugins: [],
-  };
+
+    theme: {
+        extend: {
+          animation: {
+            bounce: 'bounce 1s infinite',
+          },
+          keyframes: {
+            bounce: {
+              '0%, 100%': { transform: 'translateY(0)' },
+              '50%': { transform: 'translateY(-5px)' },
+            },
+          },
+        },
+      },
+  }
